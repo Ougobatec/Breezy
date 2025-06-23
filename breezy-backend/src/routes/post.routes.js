b2217afcd3d14b103bfbd79ed6d5fb4c79187e53
Maxime Lirio <maxime.lirio@viacesi.fr>
2025-06-23 14:10:28 +0200

    ✨ backend: add post creation (Only Text content) functionality with routes and controller

---
+++ breezy-backend/src/routes/post.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const postController = require("../controllers/post.controller");
+const authMiddleware = require('../middlewares/auth.middleware');   
+const requireFields = require('../middlewares/requiredFields.middleware');
+
+const router = express.Router();
+
+// Route pour créer un post
+router.post("/", authMiddleware, requireFields(['title', 'content']), postController.createPost);
+
+module.exports = router;
